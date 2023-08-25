11864cc5be331b518d07cd65b3f505ccaae1a3df
platformerdotio <127369979+platformerdotio@users.noreply.github.com>
2023-08-25 18:50:15 -0500

    dddddd

---
+++ js/fullscreen.js
@@ -0,0 +1,16 @@
+const reqFs = (elem) => {
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.mozRequestFullScreen) {
+    elem.mozRequestFullScreen();
+  } else if (elem.webkitRequestFullscreen) {
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) {
+    elem.msRequestFullscreen();
+  }
+};
+
+const fullscreen = () => {
+  const elem = document.getElementsByTagName("iframe")[0];
+  reqFs(elem);
+};
---
+++ js/sitelock.js
@@ -0,0 +1,1 @@
+var currentLocation = window.location;
