7acfdb181350c0a099f1b75c8fcc67dbfd148ba6
platformerdotio <127369979+platformerdotio@users.noreply.github.com>
2023-08-30 20:06:19 -0500

    ddd

---
+++ js/index.js
@@ -0,0 +1,36 @@
+var tab = localStorage.getItem('tab');
+if (tab) {
+  try {
+    var tabData = JSON.parse(tab);
+  } catch {
+    var tabData = {};
+  }
+} else {
+  var tabData = {};
+}
+
+if (tabData.title) {
+  document.title = tabData.title;
+}
+
+if (tabData.icon) {
+  document.querySelector('link[rel="icon"]').href = tabData.icon;
+}
+
+function getContrastHex(hexcolor) {
+  hexcolor = hexcolor.replace('#', '');
+  var r = parseInt(hexcolor.substr(0, 2), 16);
+  var g = parseInt(hexcolor.substr(2, 2), 16);
+  var b = parseInt(hexcolor.substr(4, 2), 16);
+  var yiq = (r * 299 + g * 587 + b * 114) / 1000;
+  return yiq >= 128 ? '#1c1c1c' : 'white';
+}
+
+function getColorHex(hexcolor) {
+  hexcolor = hexcolor.replace('#', '');
+  var r = parseInt(hexcolor.substr(0, 2), 16);
+  var g = parseInt(hexcolor.substr(2, 2), 16);
+  var b = parseInt(hexcolor.substr(4, 2), 16);
+  var yiq = (r * 299 + g * 587 + b * 114) / 1000;
+  return yiq >= 128 ? 'white' : 'black';
+}
---
+++ js/settings.js
@@ -0,0 +1,124 @@
+var tab = localStorage.getItem("tab");
+
+if (tab) {
+  try {
+    var tabData = JSON.parse(tab);
+  } catch {
+    var tabData = {};
+  }
+} else {
+  var tabData = {};
+}
+
+if (tabData.title) {
+  document.getElementById("title").value = tabData.title;
+}
+if (tabData.icon) {
+  document.getElementById("icon").value = tabData.icon;
+}
+
+var settingsDefaultTab = {
+  title: "Platformerdotio",
+  icon: "/favicon.png",
+};
+
+function setTitle(title = "") {
+  if (title) {
+    document.title = title;
+  } else {
+    document.title = settingsDefaultTab.title;
+  }
+
+  var tab = localStorage.getItem("tab");
+
+  if (tab) {
+    try {
+      var tabData = JSON.parse(tab);
+    } catch {
+      var tabData = {};
+    }
+  } else {
+    var tabData = {};
+  }
+
+  if (title) {
+    tabData.title = title;
+  } else {
+    delete tabData.title;
+  }
+
+  localStorage.setItem("tab", JSON.stringify(tabData));
+}
+
+function setFavicon(icon) {
+  if (icon) {
+    document.querySelector("link[rel='icon']").href = icon;
+  } else {
+    document.querySelector("link[rel='icon']").href = settingsDefaultTab.icon;
+  }
+
+  var tab = localStorage.getItem("tab");
+
+  if (tab) {
+    try {
+      var tabData = JSON.parse(tab);
+    } catch {
+      var tabData = {};
+    }
+  } else {
+    var tabData = {};
+  }
+
+  if (icon) {
+    tabData.icon = icon;
+  } else {
+    delete tabData.icon;
+  }
+
+  localStorage.setItem("tab", JSON.stringify(tabData));
+}
+
+function setCloak() {
+  var cloak = document.getElementById("premadecloaks").value;
+  switch (cloak) {
+    case "search":
+      setTitle("Google");
+      setFavicon("./images/cloaks/Google Search.ico");
+      location.reload();
+      break;
+    case "itchio":
+      setTitle("Top free NSFW games for web");
+      setFavicon("./images/cloaks/D23D344B-4CB0-4799-B525-F4E4F3A36728.ico");
+      location.reload();
+      break;
+    // ... (other cases removed for brevity)
+  }
+}
+
+function resetTab() {
+  document.title = settingsDefaultTab.title;
+  document.querySelector("link[rel='icon']").href = settingsDefaultTab.icon;
+  document.getElementById("title").value = "";
+  document.getElementById("icon").value = "";
+  localStorage.setItem("tab", JSON.stringify({}));
+}
+
+function setTheme(theme) {
+  localStorage.setItem("theme", theme);
+  document.body.setAttribute("theme", theme);
+  document.body.style = "";
+  localStorage.removeItem("theme_color");
+
+  themes.forEach((palette) => {
+    if (palette.theme == theme) {
+      document.querySelector("#theme_color").value = palette.color;
+    }
+  });
+}
+
+function setThemeColor(theme) {
+  localStorage.setItem("theme", "custom");
+  localStorage.setItem("theme_color", theme);
+  document.body.setAttribute("theme", "custom");
+  document.body.style = `--theme: ${theme}; --background: ${getContrastHex(theme)}; --text: ${getColorHex(theme)}; --text-secondary: ${getColorHex(theme)};`;
+}
