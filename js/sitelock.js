3bdd5ea76a926d1de0af29d408d4d41dd08ed0c2
platformerdotio <127369979+platformerdotio@users.noreply.github.com>
2023-09-05 21:59:47 -0500

    ddd

--- js/sitelock.js
+++ js/sitelock.js
@@ -1,1 +1,15 @@
-var currentLocation = window.location;
+var allowedDomains = [
+  "https://platformerdotio.co",
+  "https://platformerio.bitbucket.io/",
+  "https://platformerdotio.onrender.com",
+];
+
+var referringUrl = document.referrer;
+
+var isAllowedDomain = allowedDomains.some(function (domain) {
+  return referringUrl.indexOf(domain) !== -1;
+});
+
+if (!isAllowedDomain) {
+  window.location.href = allowedDomains[0];
+}
