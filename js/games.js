b903695ed0fb2ea4def300bfb5b288b5cc733f93
platformerdotio <127369979+platformerdotio@users.noreply.github.com>
2023-09-13 20:26:51 -0500

    dd

--- js/games.js
+++ js/games.js
@@ -3,17 +3,33 @@ const searchInput = document.getElementById("searchInput");
 
 searchInput.addEventListener("input", function () {
   const searchTerm = searchInput.value.toLowerCase();
-  const gameCards = gameList.getElementsByClassName("game-card");
+  const gameCards = gameList.querySelectorAll(".game-card");
 
-  for (const card of gameCards) {
+  gameCards.forEach((card) => {
     const title = card.querySelector(".title").textContent.toLowerCase();
     if (title.includes(searchTerm)) {
       card.style.display = "block";
     } else {
       card.style.display = "none";
     }
+  });
+  
+  gameList.style.removeProperty("display");
+  gameList.style.removeProperty("flex-wrap");
+
+  const visibleCards = Array.from(gameList.querySelectorAll(".game-card[style='display: block;']"));
+
+  if (visibleCards.length > 0) {
+    gameList.style.display = "flex";
+    gameList.style.flexWrap = "wrap";
+  } else {
+    gameList.style.display = "grid";
+    gameList.style.gridTemplateColumns = "repeat(auto-fit, 150px)";
   }
 });
+
+
+
 
 document.getElementById('randomGameButton').addEventListener('click', function () {
   const gameLinks = document.querySelectorAll('#gameList a');
