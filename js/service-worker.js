f5d6b2f5a269260363519f40b944ef4a81dced5d
platformerdotio <smartfoloo@gmail.com>
2023-09-20 21:30:06 -0500

    ddd

---
+++ js/service-worker.js
@@ -0,0 +1,43 @@
+function greet() {
+  console.log('idk wat to put here have a great day');
+}
+
+const cacheName = 'pws';
+const cacheAssets = [
+  '/',
+  '/index.html',
+  '/*'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(cacheName)
+      .then((cache) => {
+        return cache.addAll(cacheAssets);
+      })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys()
+      .then((cacheNames) => {
+        return Promise.all(
+          cacheNames.map((cache) => {
+            if (cache !== cacheName) {
+              return caches.delete(cache);
+            }
+          })
+        );
+      })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        return response || fetch(event.request);
+      })
+  );
+}
