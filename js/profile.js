d8977b4a65228111f7c80aca25d271a45fb194af
platformerdotio <smartfoloo@gmail.com>
2023-10-03 21:42:49 -0500

    fixed days

--- js/profile.js
+++ js/profile.js
@@ -1,3 +1,22 @@
+var tab = localStorage.getItem('tab');
+if (tab) {
+  try {
+    var tabData = JSON.parse(tab);
+  } catch {
+    var tabData = {};
+  }
+} else {
+  var tabData = {};
+}
+
+if (tabData.title) {
+  document.title = tabData.title;
+}
+
+if (tabData.icon) {
+  document.querySelector('link[rel="icon"]').href = tabData.icon;
+}
+
 function displayUserXP() {
   var currentXP = parseInt(localStorage.getItem("userXP")) || 0;
   var xpPerLevel = 200;
