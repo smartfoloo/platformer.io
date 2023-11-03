f9f76ff55b009b22294fd9fdc4c14ec41563f446
platformerdotio <smartfoloo@gmail.com>
2023-11-02 20:53:49 -0500

    fff

--- js/randomtext.js
+++ js/randomtext.js
@@ -1,14 +1,7 @@
 const texts = [
-  "get back to work!",
+  "dm me to host this site",
   "derek is cool",
   "join the discord!",
-  "we live we love we lie",
-  "welcome to china airline",
-  "peppa pig is blocked?",
-  "Control+Shift+Q+Q for free robux",
-  "you are chatgpt",
-  "made with help from obama",
-  "back in my day i used to walk 29 km to this classroom"
 ];
 
 function getRandomText() {
