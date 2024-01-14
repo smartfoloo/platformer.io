c9f8b0cda971e06f6d6a9698d20e1069ef9a672a
platformerdotio <smartfoloo@gmail.com>
2024-01-13 22:42:15 -0600

    fff

--- js/loader.js
+++ js/loader.js
@@ -70,6 +70,10 @@ if (gameToLoad) {
     gameframe.src = 'https://nearpod-currentlyconnectedtolesson.onrender.com/assets/yohoho/index.html';
     gamename.textContent = 'Yohoho.io';
     gameimage.src = '/images/yohohoio.avif';
+  } else if (gameToLoad === 'dadish-3') {
+    gameframe.src = 'https://dadish3.netlify.app/';
+    gamename.textContent = 'Dadish 3';
+    gameimage.src = '/images/dadish3.avif';
   } else {
     const imageName = gameToLoad.replace(/-/g, '');
     const imageSrcPng = `/images/${imageName}.png`;
